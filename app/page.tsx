b75087145b99e39b6cdce45264f901b6a32edbdb
dilla.arkademi <dilla.arkademi@gmail.com>
2023-08-16 18:00:17 +0700

    image

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,12 @@ export default function Home() {
     <>
       <main className={styles.main}>
         <div className={styles.wrapper}>
+          <Image
+            src={img}
+            width={500}
+            height={500}
+            alt="Picture of the author"
+          />
           <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat voluptas culpa, hic nulla eaque vitae tempore asperiores, recusandae iste veniam commodi quas nobis! Molestiae qui quis doloribus libero natus nam.</div>
         </div>
       </main>
