e2ba54a0baa004fdcde262b4abcb9948cc22029c
dilla.arkademi <dilla.arkademi@gmail.com>
2023-09-22 05:12:14 +0700

    app

--- components/Carousel/index.tsx
+++ components/Carousel/index.tsx
@@ -2,8 +2,6 @@ import { IcNext, IcNextRound, IcPrev, IcPrevRound } from "../assets";
 import Image from "next/image";
 import { ReactNode } from "react";
 import Slider from "react-slick";
-import "slick-carousel/slick/slick-theme.css";
-import "slick-carousel/slick/slick.css";
 
 const ArkCarousel = ({
   // komponent child / dalam bentuk loop
